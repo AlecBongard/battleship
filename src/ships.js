3db043f11e1ac0fa68e5a68854e8e3c1e4aa4691
Alec Bongard <alec.bongard@gmail.com>
2023-02-15 13:03:48 -0800

    Implement ship hit function

--- src/ships.js
+++ src/ships.js
@@ -1,9 +1,27 @@
 const ship = (length) => {
   let _hits = 0;
 
-  function _isSunk() {}
+  function _isSunk() {
+    if (_hits >= length) {
+      return true;
+    }
 
-  function hit() {}
+    return false;
+  }
+
+  function hit() {
+    _hits += 1;
+
+    if (_isSunk()) {
+      return "You sunk my battleship";
+    }
+
+    return null;
+  }
+
+  return {
+    hit,
+  };
 };
 
-module.exports = { ship };
+module.exports = ship;
